76cbdcbc18e3824b6e48ca391740fee447b65abf
Sean Sundberg <seansund@us.ibm.com>
2019-11-16 10:34:57 -0600

    Updates StockItemList view with table data

---
+++ client/src/components/StockItemList.jsx
@@ -0,0 +1,145 @@
+import React, { Component } from "react";
+import {
+  StructuredListWrapper,
+  StructuredListRow,
+  StructuredListCell,
+  StructuredListHead,
+  StructuredListBody,
+  StructuredListInput,
+  Icon
+} from "carbon-components-react";
+import { iconCheckmarkSolid } from "carbon-icons";
+import Header from "../pattern-components/Header";
+import "../pattern-components/patterns.scss";
+
+class StockItemList extends Component {
+  title = 'Stock Items';
+  subtitle = 'This is the current inventory of items';
+
+  columns = [
+    "name",
+    "description",
+    "stock",
+    "unitPrice",
+    "picture",
+    "manufacturer",
+  ];
+  formatters = {};
+  data = [
+    {
+      "name": "Item 1",
+      "description": "The first item",
+      "stock": 10,
+      "unitPrice": 100.0,
+      "picture": "test",
+      "manufacturer": "unknown",
+    },
+    {
+      "name": "Item 2",
+      "description": "The second item",
+      "stock": 15,
+      "unitPrice": 120.5,
+      "picture": "test1",
+      "manufacturer": "Apple",
+    },
+    {
+      "name": "Item 3",
+      "description": "The third item",
+      "stock": 20,
+      "unitPrice": 75.5,
+      "picture": "test1",
+      "manufacturer": "Sony",
+    }
+  ];
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      selectedRow: 0,
+    };
+  }
+
+  async componentDidMount() {
+
+    this.setState({
+      data: this.data,
+    })
+  }
+
+  onRowClick = id => {
+    this.setState({ selectedRow: id });
+  };
+
+  renderRow = (row, id) => {
+    return (
+      <StructuredListRow key={id} onClick={() => this.onRowClick(id)}>
+        <div>
+          <StructuredListInput
+            id={`row-${id}`}
+            value="row-0"
+            title="row-0"
+            name="row-0"
+            //defaultChecked={this.state.selectedRow === id}
+            checked={this.state.selectedRow === id}
+          />
+          <StructuredListCell>
+            <Icon
+              className="bx--structured-list-svg"
+              icon={iconCheckmarkSolid}
+            />
+          </StructuredListCell>
+        </div>
+        {this.columns.map(col => {
+          const format = this.formatters[col] || function(val) { return val; };
+
+          return (
+            <StructuredListCell key={col} className="simple-list-row">
+              {format(row[col])}
+            </StructuredListCell>
+          );
+        })}
+      </StructuredListRow>
+    );
+  };
+
+  render() {
+    const data = this.state.data;
+
+    return (
+      <div className="bx--grid pattern-container">
+        <Header
+          title={this.title}
+          subtitle={this.subtitle}
+        />
+        <div className="bx--row">
+          <div className="bx--col-xs-12">
+            <StructuredListWrapper selection border>
+              <StructuredListHead>
+                <StructuredListRow head>
+                  <StructuredListCell head />
+                  {this.columns.map(key => {
+                    return (
+                      <StructuredListCell head key={key}>
+                        {key.charAt(0).toUpperCase() +
+                          key.slice(1).replace(/([A-Z])/g, " $1")}
+                      </StructuredListCell>
+                    );
+                  })}
+                </StructuredListRow>
+              </StructuredListHead>
+
+              <StructuredListBody>
+                {data.map((row, i) => {
+                  return this.renderRow(row, i);
+                })}
+              </StructuredListBody>
+            </StructuredListWrapper>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default StockItemList;
