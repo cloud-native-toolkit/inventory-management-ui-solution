0d8c1417ae58ad951744960e684772d36c657c94
Carlos Santana <csantana@us.ibm.com>
2021-03-04 21:47:17 -0500

    update with istio labels (#24)
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- client/src/components/UIShell.jsx
+++ client/src/components/UIShell.jsx
@@ -23,7 +23,7 @@ const Fade20 = () => (
 );
 
 class UIShell extends Component {
-  header = "Carlos Inc.";
+  header = "Inventory";
   menuTitle = "Inventory Management";
   menuItems = [
     "Stock Items",
